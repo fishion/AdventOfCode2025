46a1f1562d27e8e17c33919435e6f54144893faa
Alex <alex@monney.me>
2025-12-03 19:29:48 +0000

    Day 3 pt2 + refactor

--- day03/index.ts
+++ day03/index.ts
@@ -7,26 +7,41 @@ class Puzzle extends PuzzleBase {
     super(day)
   }
 
-  pt1(){
-    this.input.forEach((bank: string) => {
-      let d1 = -1
-      let d2 = -1
-      const jolts: number[] = bank.split('').map((jolt: string) => parseInt(jolt))
-      for (let i=0; i<jolts.length; i++) { // could maybe be more efficient going backwards?
-        if (i<jolts.length-1 && jolts[i] > d1){
-          d1 = jolts[i]
-          d2 = jolts[i+1]
-        } else if (jolts[i] > d2){
-          d2 = jolts[i]
+  bankJoltage(bank: string, qtyBatteriesEnabled: number = 2){
+    const output = new Array(qtyBatteriesEnabled).fill(-1)
+    const cells: number[] = bank.split('').map((jolt: string) => parseInt(jolt))
+    for (let cIndex=0; cIndex<cells.length; cIndex++) { 
+      const cellsLeft = cells.length - cIndex;
+      // loop through output digits and see what should be updated
+      for (let oIndex=0; oIndex<=qtyBatteriesEnabled; oIndex++){
+        const outputsLeft = output.length - oIndex
+        if (cellsLeft < outputsLeft) continue // this cell can't fill this output slot - not enough outputs left
+        if (cells[cIndex] > output[oIndex]){
+          output[oIndex] = cells[cIndex] // This is new highest joltage for cell in this position
+          output.fill(-1, oIndex+1) // All following digits will have to change. Reset them to -1
+          break // Already applied this cell to an output digit. Move on.
         }
       }
-      if (d1 < 0 || d2 < 0){ throw Error(`Didn't find a d1 or d2 : ${d1} : ${d2}`) }
-      this.debugOut(`joltage = ${d1}${d2}`)
-      this.ans.pt1 = <number>this.ans.pt1 + parseInt(`${d1}${d2}`)
+    }
+    if (output[0] < 0 || output[1] < 0){ throw Error(`Didn't find a d1 or d2 : ${output[0]} : ${output[1]}`) }
+    return parseInt(output.join(''))
+  }
+
+  totalJoltage(qtyBatteriesEnabled: number = 2){
+    let total = 0;
+    this.input.forEach((bank: string) => {
+      const joltage = this.bankJoltage(bank, qtyBatteriesEnabled)
+      this.debugOut(`joltage for bank ${bank} = ${joltage}`)
+      total += joltage
     })
+    return total
+  }
+
+  pt1(){
+    this.ans.pt1 = this.totalJoltage(2)
   }
   pt2(){
-
+    this.ans.pt2 = this.totalJoltage(12)
   }
 
 }
